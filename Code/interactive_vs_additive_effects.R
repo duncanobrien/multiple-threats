2e843f01a53f735796502e021d7f255ea07a33ae
duncanobrien <duncan.a.obrien@gmail.com>
2024-02-29 10:02:59 +0000

    update interactive vs additive effects

--- Code/interactive_vs_additive_effects.R
+++ Code/interactive_vs_additive_effects.R
@@ -1,187 +1,14 @@
 require(brms)
 require(tidyverse)
-load("Data/LivingPlanetData2.RData")
 
-priors <- c(prior(normal(0, 1), class = b),
-            prior(exponential(1), class = sd),
-            #prior(exponential(1), class = sigma),
-            prior(normal(0,0.25), class = ar))
-
-####################
-# raw data taken from PolCap repository
-####################
-dd_long2 <- dd_long %>%
-  group_by(ID) %>%  
-  # Calculate population change
-  mutate(popchange=log(Count+(max(Count, na.rm=T) /100)),
-         #ifelse(Count<1, log(Count/(max(Count,na.rm = T))*100+1), 
-         #        log(Count+1)),
-         Protected_status=gsub(" .*", "", Protected_status)) %>% 
-  # Remove any groupings we have created in the pipe
-  ungroup()
-
-# Add the biogeographical region
-
-dd_long2 <- dd_long2 %>%
-  mutate(T_realm=na_if(T_realm, "NULL"),
-         M_realm=na_if(M_realm, "NULL"),
-         FW_realm=na_if(FW_realm, "NULL"),
-         Realm=coalesce(T_realm, M_realm, FW_realm))
-
-# Find the data ids
-
-dd_final <- dd_long2 %>%
-  group_by(ID) %>%
-  drop_na(popchange) %>%
-  mutate(start=min(Year),
-         end=max(Year),
-         Duration=end-start) %>% 
-  filter(Duration>9)
-
-# Spread the data again
-
-pops <- dd_final %>%
-  mutate(year = as.factor(as.character(Year))) %>% 
-  dplyr::select(ID, SpeciesName, Class, Order,
-                System, Latitude, Longitude,
-                Region,
-                Protected_status, n.threat, 
-                Primary_threat,
-                Secondary_threat,
-                Tertiary_threat,
-                Managed,
-                Realm,
-                threats, Duration, 
-                Year, year, popchange) %>%
-  drop_na(popchange) %>%
-  group_by(ID) %>%
-  dplyr::select(-Year) %>%
-  filter(length(unique(year))>=5) %>% 
-  pivot_wider(names_from = year, values_from = popchange)
-
-load("Data/SSResults.RData")
-
-pop_data10 <- pop_data %>% # pop10 selected following original repository
-  mutate(Year=as.numeric(Year)) %>% 
-  group_by(ID) %>% 
-  filter(length(unique(Year))>=10) %>%
-  select(ID,pollution,habitatl,climatechange,
-         invasive, exploitation,disease) %>%
-  group_by(ID) %>%
-  mutate(none = ifelse(all(is.na(c(pollution,habitatl,climatechange,
-                                   invasive, exploitation,disease))),
-                       "none",NA)) %>%
-  filter(ID %in% pops$ID) %>%
-  distinct()
-
-####################
-# further wrangling in to long format and prepare explanatory variables
-####################
-mod_dat_full <- pops %>%
-  pivot_longer(c("1950":"2019"), names_to = "year", values_to = "y") %>%
-  mutate(year = as.numeric(year),
-         #time = seq_along(year),
-         series = paste(ID), #factor required for autocorrelation estimation
-         threats = factor(ifelse(is.na(threats),"none",threats))) %>% #convert stressors to binary
-  mutate(threats = fct_relevel(threats, "none")) %>%
-  left_join(select(pop_data,c(ID,pollution,habitatl,climatechange,
-                              invasive, exploitation,disease)),
-            multiple = "first",by = "ID") %>% #double check. Currently NOT selected timeseries >= 10 years
-  mutate(none = ifelse(all(is.na(c(pollution,habitatl,climatechange,
-                                   invasive, exploitation,disease))),
-                       "none",NA)) %>%
-  mutate(across(pollution:none,~ifelse(is.na(.x),"0","1"))) %>% #convert absence of stress to binary
-  group_by(ID) %>%
-  slice(1:max(which(!is.na(y))))  %>% #remove lagging years containing all NAs (to prevent unbounded interpolation)
-  mutate(scaled_year = c(scale(year,center = TRUE,scale = FALSE)), #center time to 0 for each timeseries
-         time = seq_along(year),
-         scaled_time = c(scale(time,center = TRUE,scale = FALSE)),
-         #y_centered = log(y/max(na.omit(y))), #rescale y by maximum of timeseries and log
-         y_centered = y-(na.omit(y)[1]-0) #recenter y so that first value of timeseries is 0 (to allow all intercepts to be removed)
-  ) %>% 
-  ungroup(ID) %>%
-  as.data.frame()
-
-test_data3 <-  mod_dat_full %>% 
-  drop_na(y) %>%
-  #filter(series %in% base::sample(unique(series),500)) %>%
-  mutate(threats = as.character(threats)) 
-
-ggplot(subset(test_data3,ID %in% sample(ID,5)),aes(x=scaled_time,y=y_centered,col= factor(ID))) + geom_line()
-
-##########################################################################################
-## Tom suggestion interactive vs additive ##
-##########################################################################################
-thrts_2 <-combn(c("pollution","habitatl","climatechange","invasive", "exploitation","disease"),2) #create all unique two way combinations of threats
-thrts_3 <-combn(c("pollution","habitatl","climatechange","invasive", "exploitation","disease"),3) #create all unique three way combinations of threats
-
-test_data2 <-  mod_dat_full %>% 
-  drop_na(y) #double check how to deal with infrequently sampled timeseries. Current model is able to deal via the ar process
-#filter(series %in% base::sample(unique(series),500)) %>%
-
-intvadd_dat2 <- test_data2 %>% #create a new dataframe with columns containing "0"/"1" for each combination of threats. "0" = combination not present, "1" = combination present
-  bind_cols(purrr::pmap_dfc(.l = list(.x = thrts_2[1,], #threat 1
-                                      .y = thrts_2[2,]),#threat 2
-                            ~ test_data2 %>%
-                              select({{.x}},{{.y}},ID) %>% #select just the necessary columns (threat 1, threat 2 and timeseries ID)
-                              mutate(!!sym(paste(.x,.y,sep=".")) :=  
-                                       ifelse(all(!!sym(.x) == "1") & all(!!sym(.y) == "1"),"1","0"),
-                                     .by = ID) %>% #dynamically create new column of whether both threat 1 and threat 2 are 1's
-                              select(4))%>%
-              select_if(function(x)sum(x == "1") > 0) #drop columns where no observed combination of threats as will prevent model fitting
-  ) %>%
-  bind_cols(purrr::pmap_dfc(.l = list(thrts_3[1,], #threat 1
-                                      thrts_3[2,], #threat 2
-                                      thrts_3[3,]),#threat 3
-                            function(.x,.y,.z) test_data2 %>%
-                              select({{.x}},{{.y}},{{.z}},ID) %>% #select just the necessary columns (threat 1, threat 2 and timeseries ID)
-                              mutate(!!sym(paste(.x,.y,.z,sep=".")) :=  
-                                       ifelse(all(!!sym(.x) == "1") & all(!!sym(.y) == "1") & all(!!sym(.z) == "1"),"1","0"),
-                                     .by = ID) %>% #dynamically create new column of whether both threat 1, threat 2 and threat 3 are 1's
-                              select(5)) %>%
-              select_if(function(x)sum(x == "1") > 0) #drop columns where no observed combination of threats
-  ) 
-
-# intvadd_dat3 <- subset(intvadd_dat2,ID %in% sample(ID,100)) %>%
-#   select_if(function(x)!all(x == "0"))
-
-intvadd_dat3 <-intvadd_dat2 %>%
-  group_by(ID) %>% 
-  filter(length(unique(year))>=5) %>% #filter to timeseries > 5 time points
-  ungroup() %>%
-  select_if(function(x)!all(x == "0"))
-
-rhs <- paste0(paste("scaled_year*",
-                    colnames(intvadd_dat3)[
-                      grepl(paste(c("pollution","habitatl","climatechange","invasive", "exploitation","disease"),
-                                  collapse = "|"),
-                            colnames(intvadd_dat3))],
-                    sep ="",collapse = " + "),
-              " + (-1 + scaled_year|SpeciesName) + (-1 + scaled_year|series) + -1") #create the right hand side of the model formula with interactions between scaled_year and all threat combinations
-form <- as.formula(paste("y_centered", "~", rhs)) #combine y_centered and rhs into a model formula
-
-mod_intvadd2 <- brm(bf(form #include realm/spp as slopes, x intercepts
-                       ,autocor = ~ar(time = time,gr = series,p=1) #ou/arima process
-                       ),
-                    data = intvadd_dat3, 
-                    family = gaussian(),
-                    iter = 8000,
-                    refresh=100,
-                    #backend = "cmdstanr",
-                    prior = priors,
-                    chains = 4,
-                    control=list(adapt_delta=0.975,max_treedepth = 10),
-                    cores = 4)
-saveRDS(mod_intvadd2,"Results/models/simple-slopes-intvadd3.RDS")
-mod_intvadd2 <- readRDS("Results/models/simple-slopes-intvadd2.RDS")
-mod_intvadd3 <- readRDS("Results/models/simple-slopes-intvadd3.RDS")
+m1 <- read_rds("Results/models/mod_global.RDS")
 
 source("Code/prep_data_grid_fn.R")
 source("Code/threat_post_draws.R")
 
 all_threats = c("pollution","habitatl","climatechange","invasive", "exploitation","disease")
 
-threatcols <- colnames(mod_intvadd3$data)[grepl(paste(all_threats,collapse = "|"),colnames(mod_intvadd3$data))] 
+threatcols <- colnames(m1$data)[grepl(paste(all_threats,collapse = "|"),colnames(m1$data))] 
 
 additive_cols <- do.call("c",lapply(strsplit(threatcols,"[.]"),function(x){
   paste(x,collapse = " + ")
@@ -189,7 +16,7 @@ additive_cols <- do.call("c",lapply(strsplit(threatcols,"[.]"),function(x){
 
 target_cols <- unique(c(threatcols,additive_cols))
 
-postdraws_intvadd <- threat_post_draws(model = mod_intvadd3,
+postdraws_intvadd <- threat_post_draws(model = m1,
                                        threat_comns = target_cols,
                                        nuisance = c("series","SpeciesName"),
                                        n.cores = 4) %>%
@@ -259,6 +86,9 @@ post_interval_intvadd_diff <- post_intvadd_diff %>%
     TRUE ~ "additive"
   ))
 
+#######
+#Generate Figure 3
+#######
 ggplot(data = na.omit(post_intvadd_diff), 
        aes(x = .value,y=threats)) +
   tidybayes::stat_slab(data = na.omit(post_intvadd_diff) %>%
